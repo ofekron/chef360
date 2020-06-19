edbec22629781063c7f15fdbd772532a43253e94
Ofek <ofek@pickapp.farm>
2020-06-19 16:03:36 +0300

    adding db

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ from utils import version
 from v1.api import blueprint as v1
 from v2.api import blueprint as v2
 
-
-
-
+print(os.environ['DATABASE_URL'])
 
 app = Flask(__name__)
 app.register_blueprint(v1)
@@ -29,5 +27,5 @@ def redirect_latest():
 def index(p):
     return Response(json.dumps(['%s' % rule for rule in app.url_map.iter_rules()]), mimetype='application/json')
 if __name__ == '__main__':
-    print(os.environ['DATABASE_URL'])
+
     app.run()
